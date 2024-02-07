886aa0b9ba154d81572d4965e1c20988debcf6a6
Jessye Pereira <jessyekessia16@gmail.com>
2024-02-06 23:45:59 -0300

    Subindo código

---
+++ src/components/note-card.tsx
@@ -0,0 +1,20 @@
+// propriedades
+interface NoteCarProps {
+    note: {
+        date: Date
+        content: string
+    }
+}
+export function NoteCard ({ note }: NoteCarProps) {
+    return (
+    <button className="rouded-nd flex flex-col text-left bg-slate-800 p-5 gap-3 relative overflow-hidden outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-2 focus-visible:ring-line-400">
+        <span className='text-sm font-medium text-slate-300'> 
+            {note.date.toISOString()}
+        </span>
+        <p className="text-sm leading-6 text-slate-400">   
+            {note.content}
+        </p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0"></div>
+    </button>
+    )
+}
